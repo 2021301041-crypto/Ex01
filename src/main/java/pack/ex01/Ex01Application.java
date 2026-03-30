bf5ed7d186106f5b59a7e78d181139322f5318c7
2021301041-crypto <2021301041@skuniv.ac.kr>
2026-03-30 10:47:35 +0900

    09:45

---
+++ src/main/java/pack/ex01/Ex01Application.java
@@ -0,0 +1,22 @@
+package pack.ex01;
+
+import org.apache.tomcat.util.net.jsse.JSSEUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Ex01Application {
+
+    @GetMapping("/")
+    public String hello() {
+        System.out.println("Ex01");
+        return "Ex01 project: Am 09:21";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex01Application.class, args);
+    }
+}
